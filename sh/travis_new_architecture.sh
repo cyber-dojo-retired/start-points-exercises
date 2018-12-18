3fd8b26946c31e4def69186026b4943b2b39c028
Jon Jagger <jon@jaggersoft.com>
2018-12-18 13:55:48 +0000

    give all 3 repo names to builder script

--- sh/travis_new_architecture.sh
+++ sh/travis_new_architecture.sh
@@ -2,14 +2,11 @@
 set -ev
 
 readonly MY_TYPE=exercises
-
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT}
-
 readonly MY_IMAGE_NAME=cyberdojo/start-points-${MY_TYPE}-test
-readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-${MY_TYPE}.git
-
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-${MY_TYPE}.XXXXXXXXX)
+
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
 
@@ -19,4 +16,6 @@ chmod +x ./${SCRIPT}
 
 ./${SCRIPT} \
   ${MY_IMAGE_NAME} \
-  ${MY_REPO_NAME}
+  https://github.com/cyber-dojo/start-points-${MY_TYPE}.git \
+  https://github.com/cyber-dojo/start-points-custom.git     \
+  https://github.com/cyber-dojo/start-points-languages.git  \
