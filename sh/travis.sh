066400e3e14dfbd5365f555097e20035fd559fc0
Jon Jagger <jon@jaggersoft.com>
2018-12-18 13:06:37 +0000

    it seems the json marker file has to be in the root dir

--- sh/travis.sh
+++ sh/travis.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -ev
 
-# todo: MY_REPO_NAME is this repo
-# passing it as file:///... based url would make things a bit quicker
+readonly my_type=exercises
 
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT}
 
-readonly MY_IMAGE_NAME=cyberdojo/start-points-exercises-test
-readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-exercises.git
+readonly MY_IMAGE_NAME=cyberdojo/start-points-${my_type}-test
+readonly MY_REPO_NAME=https://github.com/cyber-dojo/start-points-${my_type}.git
 
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-exercises.XXXXXXXXX)
+readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-${my_type}.XXXXXXXXX)
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
 
