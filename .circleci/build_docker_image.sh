c308017787eecb902a5cea4fa9c97f988bcfee6d
Jon Jagger <jon@jaggersoft.com>
2019-01-25 12:26:25 +0000

    fix ci scripts

--- .circleci/build_docker_image.sh
+++ .circleci/build_docker_image.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 set -ev
 
-readonly MY_TYPE=exercises
-readonly SCRIPT=build_cyber_dojo_start_points_image.sh
-readonly URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT}
-readonly MY_IMAGE_NAME=cyberdojo/start-points-${MY_TYPE}-test
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-${MY_TYPE}.XXXXXXXXX)
-
-cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
-trap cleanup EXIT
-
-cd ${TMP_DIR}
-curl -O ${URL}
-chmod +x ./${SCRIPT}
-
-./${SCRIPT} \
-  ${MY_IMAGE_NAME} \
-    https://github.com/cyber-dojo/start-points-${MY_TYPE}.git \
-    https://github.com/cyber-dojo/start-points-custom.git     \
-    https://github.com/cyber-dojo/start-points-languages.git  \
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+"${MY_DIR}/../_ci_scripts/new_architecture.sh"
