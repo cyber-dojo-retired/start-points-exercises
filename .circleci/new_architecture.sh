46607d052f175ebb985b8519f001d6e3866cf0c9
Jon Jagger <jon@jaggersoft.com>
2019-01-29 13:12:41 +0000

    tweak ci script so it prints each git-repo-url as it is processed

--- .circleci/new_architecture.sh
+++ .circleci/new_architecture.sh
@@ -6,8 +6,8 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly SCRIPT_NAME=build_cyber_dojo_start_points_image.sh
 readonly SCRIPT_URL=https://raw.githubusercontent.com/cyber-dojo/start-points-base/master/${SCRIPT_NAME}
 
-readonly LANGUAGE_LIST=languages_list
-readonly LANGUAGE_LIST_URL=https://raw.githubusercontent.com/cyber-dojo/start-points-languages/master/${LANGUAGE_LIST}
+readonly LANGUAGE_LIST_FILENAME=languages_list
+readonly LANGUAGE_LIST_URL=https://raw.githubusercontent.com/cyber-dojo/start-points-languages/master/${LANGUAGE_LIST_FILENAME}
 
 readonly IMAGE_NAME=cyberdojo/start-points
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points.XXXXXXXXX)
@@ -19,11 +19,12 @@ cd ${TMP_DIR}
 curl -O ${SCRIPT_URL}
 chmod 700 ./${SCRIPT_NAME}
 curl -O ${LANGUAGE_LIST_URL}
+readonly LANGUAGE_LIST="$(< ./${LANGUAGE_LIST_FILENAME})"
 
 ./${SCRIPT_NAME} \
     ${IMAGE_NAME} \
     --languages \
-      "$(< ./${LANGUAGE_LIST})" \
+      "${LANGUAGE_LIST}" \
     --custom \
       https://github.com/cyber-dojo/start-points-custom.git \
     --exercises \
